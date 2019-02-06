8ca0a5ed655c60b7b95991880b22326620374b39
psspratik <psspratik@gmail.com>
2019-02-06 15:23:11 +0530

    Update Problem1.cpp

--- C-Programming/Assignment2/Problem1.cpp
+++ C-Programming/Assignment2/Problem1.cpp
@@ -15,9 +15,26 @@ void binary_to_decimal()
 	printf("decimal number = %d",decimal);
 	getchar();
 }
+
+void decimal_to_binary()
+{
+	int binary = 0, decimal = 0, i = 1;
+	printf("Enter decimal number: \n");
+	scanf_s("%d", &decimal);
+	while (decimal)
+	{
+		binary = binary + (decimal % 2)*(i);
+		i = i * 10;
+		decimal /= 2;
+	}
+	printf("binary number = %d\n", binary);
+	getchar();
+}
+
 int main()
 {
 	binary_to_decimal();
+	decimal_to_binary();
 	getchar();
 	return 0;
 }
